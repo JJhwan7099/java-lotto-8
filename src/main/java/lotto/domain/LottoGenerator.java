67e25aeb324189e380f21f8a4e26664221d50ed1
JJhwan7099 <ghks7099@naver.com>
2025-10-29 19:02:34 +0900

    feat(LottoGenerator.java): 로또 생성 클래스 LottoGenerator 구현
    - generateRandomLottoNumbers() 로또 랜덤 번호 6개 생성 함수
    - generateLottos() 로또 list 생성 및 반환 함수

---
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -0,0 +1,31 @@
+package lotto.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import lotto.domain.parser.InputParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LottoGenerator {
+    private static final int LOTTO_NUMBER_MIN = 1;
+    private static final int LOTTO_NUMBER_MAX = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+    private static final int LOTTO_PRICE = 1000;
+    private final int totalPurchaseAmount;
+
+    public LottoGenerator(TotalPurchaseAmount totalPurchaseAmount) {
+        this.totalPurchaseAmount = totalPurchaseAmount.getValue();
+    }
+
+    public List<Lotto> generateLottos() {
+        List<Lotto> lottos = new ArrayList<>();
+        for(int i = 0; i < totalPurchaseAmount/LOTTO_PRICE; i++) {
+            lottos.add(new Lotto(generateRandomLottoNumbers()));
+        }
+        return lottos;
+    }
+
+    private List<Integer> generateRandomLottoNumbers() {
+        return Randoms.pickUniqueNumbersInRange(LOTTO_NUMBER_MIN, LOTTO_NUMBER_MAX, LOTTO_NUMBER_COUNT);
+    }
+}
