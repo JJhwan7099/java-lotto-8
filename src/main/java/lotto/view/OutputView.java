a911896323b4dc5be1e4edcbad8aa6da8a0d16d7
JJhwan7099 <ghks7099@naver.com>
2025-10-29 19:22:57 +0900

    feat(OutputView.java): 발행된 로또 출력 함수 구현
    - 발행된 로또 개수 출력
    - 발행된 로또 번호 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,7 +1,18 @@
 package lotto.view;
 
+import lotto.domain.Lotto;
+
+import java.util.List;
+
 public class OutputView {
     public void printRequestTotalPurchaseAmount() {
         System.out.println("구입금액을 입력해 주세요.");
+    }
+
+    public void printPurchasedLottos(List<Lotto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+        for (Lotto lotto : lottos) {
+            System.out.println(String.join(", ", lotto.getNumbers().toString()));
+        }
     }
 }
