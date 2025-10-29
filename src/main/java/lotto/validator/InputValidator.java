b10fae6aab8591144713e10e3ffd5dd31f96dd54
JJhwan7099 <ghks7099@naver.com>
2025-10-29 19:49:02 +0900

    feat(InputValidator.java): 로또 당첨번호 입력값 검증사항 수정

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -1,9 +1,17 @@
 package lotto.validator;
 
+import java.util.List;
+
 public class InputValidator {
+
     public static void validateTotalPurchaseAmount(String totalPurchaseAmount) {
         if (!totalPurchaseAmount.matches("\\d+") || totalPurchaseAmount.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야합니다.");
+            throw new IllegalArgumentException("[ERROR] 숫자를 입력해야 합니다.");
         }
+    }
+
+    public static void validateLottoWinningNumbers(List<Integer> lottoWinningNumbers) {
+        if (lottoWinningNumbers.size() != 6)
+            throw new IllegalArgumentException("[ERROR] 로또 당첨 번호는 6개여야 합니다.");
     }
 }
