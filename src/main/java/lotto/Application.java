0c7e64709755b4525ef912ff343e9b073bae99c4
JJhwan7099 <ghks7099@naver.com>
2025-11-03 01:16:42 +0900

    feat(Application.java): LottoController에 의존성 주입 및 실행로직 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,23 @@
 package lotto;
 
+import lotto.controller.LottoController;
+import lotto.domain.LottoGenerator;
+import lotto.domain.LottoResult;
+import lotto.domain.Lottos;
+import lotto.domain.parser.InputParser;
+import lotto.view.InputView;
+import lotto.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputParser inputParser = new InputParser();
+        Lottos lottos = new Lottos();
+        LottoGenerator lottoGenerator = new LottoGenerator(lottos);
+        LottoController lottoController = new LottoController(inputView, outputView, inputParser, lottos, lottoGenerator);
+        lottoController.run();
     }
 }
