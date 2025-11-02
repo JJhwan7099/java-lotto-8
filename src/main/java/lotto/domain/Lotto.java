6ae4661e4a31405917bf6073fde72cf467524eff
JJhwan7099 <ghks7099@naver.com>
2025-11-03 03:31:36 +0900

    feat(Lotto.java): Lotto 검증 로직 추가
    - 숫자 중복 검증
    - 숫자 범위 검증
    - 숫자 개수 검증

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import java.util.HashSet;
 import java.util.List;
+
+import static lotto.domain.LottoConstraints.LOTTO_NUMBER_MAX;
+import static lotto.domain.LottoConstraints.LOTTO_NUMBER_MIN;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -11,12 +15,31 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateDuplicates(numbers);
+        validateSize(numbers);
+        validateNumberRange(numbers);
+    }
+
+    private void validateDuplicates(List<Integer> numbers) {
+        HashSet<Integer> set = new HashSet<>(numbers);
+        if(set.size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+        }
+    }
+
+    private void validateSize(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
     }
 
-    // TODO: 추가 기능 구현
+    private void validateNumberRange(List<Integer> numbers) {
+        for(Integer number : numbers) {
+            if(number < LOTTO_NUMBER_MIN || number > LOTTO_NUMBER_MAX)
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1~45 사이의 정수입니다.");
+        }
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }
