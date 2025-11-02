6cd0bdfa85f21216a7d503146686fafa5bdd5e85
JJhwan7099 <ghks7099@naver.com>
2025-11-03 01:13:01 +0900

    feat(OutputView.java): 결과 출력 함수 구현
    - 등수별 당첨 개수 출력함수 구현
    - 수익률 출력함수 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,12 @@
 package lotto.view;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoRank;
+import lotto.domain.LottoResult;
+import lotto.domain.TotalPurchaseAmount;
 
 import java.util.List;
+import java.util.Map;
 
 public class OutputView {
     public void printRequestTotalPurchaseAmount() {
@@ -10,21 +14,38 @@ public class OutputView {
     }
 
     public void printRequestLottoWinningNumbers() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println("\n당첨 번호를 입력해 주세요.");
     }
 
     public void printRequestLottoBonusNumber() {
-        System.out.println("보너스 번호를 입력해 주세요.");
+        System.out.println("\n보너스 번호를 입력해 주세요.");
     }
 
     public void printPurchasedLottos(List<Lotto> lottos) {
-        System.out.println(lottos.size() + "개를 구매했습니다.");
+        System.out.println("\n" + lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             System.out.println(String.join(", ", lotto.getNumbers().toString()));
         }
     }
 
+    public void printLottoResults(Map<LottoRank, Integer> result) {
+        System.out.println("\n당첨 통계\n---");
+        for(Map.Entry<LottoRank, Integer> entry : result.entrySet()) {
+            if(entry.getKey() == LottoRank.NONE) continue;
+            System.out.print(entry.getKey().getMatchedCount() + "개 일치");
+            if(entry.getKey().isBonusMatched()) System.out.print(", 보너스 볼 일치");
+            System.out.printf(" (%,d원) - ", entry.getKey().getPrizeAmount());
+            System.out.println(entry.getValue() + "개");
+        }
+    }
+
+    public void printProfitRate(int totalPrize, int totalPurchaseAmount) {
+        float profitRate = (float) totalPrize / totalPurchaseAmount * 100;
+        profitRate = (float) Math.round(profitRate * 100) / 100;
+        System.out.println("총 수익률은 " + profitRate + "%입니다.");
+    }
+
     public void printErrorMessage(String errorMessage) {
-        System.out.println("[ERROR] " + errorMessage);
+        System.out.println(errorMessage);
     }
 }
