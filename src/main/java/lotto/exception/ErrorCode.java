e834dc5dee0ee94226fba7fea971871de3443bd9
JJhwan7099 <ghks7099@naver.com>
2025-11-03 04:18:47 +0900

    feat(ErrorCode.java): 당첨 번호 중복에 예외 추가

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -7,6 +7,7 @@ public enum ErrorCode {
     BONUS_NUMBER_RANGE_INVALID("[ERROR] 보너스 번호는 1~45 사이의 정수입니다."),
     BONUS_NUMBER_DUPLICATE("[ERROR] 보너스 번호와 당첨 번호는 중복될 수 없습니다."),
     WINNING_NUMBER_RANGE_INVALID("[ERROR] 로또 당첨 번호는 1~45 사이의 정수입니다."),
+    WINNING_NUMBER_DUPLICATE("[ERROR] 당첨 번호는 중복되지 않아야 합니다."),
     TOTAL_PURCHASE_AMOUNT_UNIT_INVALID("[ERROR] 로또 총 구입금액은 1000원 단위입니다."),
     TOTAL_PURCHASE_AMOUNT_RANGE_INVALID("[ERROR] 로또 구입 금액은 1000원 이상이어야 합니다.");
 
