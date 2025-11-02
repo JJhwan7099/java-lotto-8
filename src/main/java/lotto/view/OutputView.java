d49961f07075fa4e868712622e37ef0cb522404d
JJhwan7099 <ghks7099@naver.com>
2025-11-02 21:47:49 +0900

    feat: 당첨 번호 입력 요청, 보너스 번호 입력 요청, 에러 메시지 출력 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -9,10 +9,22 @@ public class OutputView {
         System.out.println("구입금액을 입력해 주세요.");
     }
 
+    public void printRequestLottoWinningNumbers() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+    }
+
+    public void printRequestLottoBonusNumber() {
+        System.out.println("보너스 번호를 입력해 주세요.");
+    }
+
     public void printPurchasedLottos(List<Lotto> lottos) {
         System.out.println(lottos.size() + "개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             System.out.println(String.join(", ", lotto.getNumbers().toString()));
         }
+    }
+
+    public void printErrorMessage(String errorMessage) {
+        System.out.println("[ERROR] " + errorMessage);
     }
 }
