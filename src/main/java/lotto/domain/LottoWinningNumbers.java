45813652c00d60d28f0caaaa437d08e8df8d0ce4
JJhwan7099 <ghks7099@naver.com>
2025-11-02 23:54:19 +0900

    feat(LottoWinningNumbers.java): 일치하는 번호 개수와 보너스 번호 일치 여부 반환 함수 구현
    - getMatchedCount(Lotto lotto) : 당첨 번호와 일치하는 번호의 개수 반환
    - getIsBonusMatched(Lotto lotto) : 보너스 번호 일치여부 반환

--- src/main/java/lotto/domain/LottoWinningNumbers.java
+++ src/main/java/lotto/domain/LottoWinningNumbers.java
@@ -13,6 +13,22 @@ public class LottoWinningNumbers {
         this.bonusNumber = bonusNumber;
     }
 
+    public int getMatchedCount(Lotto lotto) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        int matchedCount = 0;
+
+        for(Integer number: lottoNumbers) {
+            if(winningNumbers.contains(number)) matchedCount++;
+        }
+
+        return matchedCount;
+    }
+
+    public boolean getIsBonusMatched(Lotto lotto) {
+        List<Integer> lottoNumbers = lotto.getNumbers();
+        return lottoNumbers.contains(bonusNumber);
+    }
+
     private void validateBonusNumber(int bonusNumber) {
         if(bonusNumber < 1 || bonusNumber > 45) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
