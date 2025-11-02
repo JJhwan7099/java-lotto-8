5dc69f59c7611edd253d2edf0f64d3c68b53bb34
JJhwan7099 <ghks7099@naver.com>
2025-11-03 02:12:23 +0900

    feat(LottoRank.java): enum LottoRank에 description 필드 추가

--- src/main/java/lotto/domain/LottoRank.java
+++ src/main/java/lotto/domain/LottoRank.java
@@ -1,35 +1,27 @@
 package lotto.domain;
 
 public enum LottoRank {
-    NONE(0, false, 0),
-    FIFTH(3, false, 5000),
-    FOURTH(4, false, 50000),
-    THIRD(5, false, 1500000),
-    SECOND(5, true, 30000000),
-    FIRST(6, false, 2000000000);
-
-
-
-
-
-
+    NONE(0, false, 0 ,""),
+    FIFTH(3, false, 5000, "3개 일치"),
+    FOURTH(4, false, 50000, "4개 일치"),
+    THIRD(5, false, 1500000, "5개 일치"),
+    SECOND(5, true, 30000000, "5개 일치, 보너스 볼 일치"),
+    FIRST(6, false, 2000000000,"6개 일치");
 
     private final int matchedCount;
     private final boolean isBonusMatched;
     private final int prizeAmount;
+    private final String description;
 
-    LottoRank(int matchedCount, boolean isBonusMatched, int prizeAmount) {
+    LottoRank(int matchedCount, boolean isBonusMatched, int prizeAmount, String description) {
         this.matchedCount = matchedCount;
         this.isBonusMatched = isBonusMatched;
         this.prizeAmount = prizeAmount;
+        this.description = description;
     }
 
-    public int getMatchedCount() {
-        return matchedCount;
-    }
-
-    public boolean isBonusMatched() {
-        return isBonusMatched;
+    public String getDescription() {
+        return description;
     }
 
     public int getPrizeAmount() {
