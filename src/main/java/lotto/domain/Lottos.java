cf8af054bd2a495ad4062560483397e784aa93f6
JJhwan7099 <ghks7099@naver.com>
2025-11-02 21:20:55 +0900

    feat: 발행한 Lotto를 저장하는 Lottos 구현

---
+++ src/main/java/lotto/domain/Lottos.java
@@ -0,0 +1,20 @@
+package lotto.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lottos {
+    private final List<Lotto> lottos = new ArrayList<>();
+
+    public Lottos() {
+
+    }
+
+    public void add(Lotto lotto) {
+        lottos.add(lotto);
+    }
+
+    public List<Lotto> gotAllLottos() {
+        return lottos;
+    }
+}
