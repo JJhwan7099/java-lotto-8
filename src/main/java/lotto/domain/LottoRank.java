73ac46df9cacdf087b39ed862f6d99c1c656163d
JJhwan7099 <ghks7099@naver.com>
2025-11-02 23:16:01 +0900

    feat(LottoRank.java): 로또의 등수별 조건과 상금 규칙을 다루는 enum LottoRank 구현

---
+++ src/main/java/lotto/domain/LottoRank.java
@@ -0,0 +1,34 @@
+package lotto.domain;
+
+public enum LottoRank {
+    FIRST(6, false, 2000000000),
+    SECOND(5, true, 30000000),
+    THIRD(5, false, 1500000),
+    FOURTH(4, false, 50000),
+    FIFTH(3, false, 5000),
+    NONE(0, false, 0);
+
+
+    private final int matchedCount;
+    private final boolean isBonusMatched;
+    private final int prizeAmount;
+
+    LottoRank(int matchedCount, boolean isBonusMatched, int prizeAmount) {
+        this.matchedCount = matchedCount;
+        this.isBonusMatched = isBonusMatched;
+        this.prizeAmount = prizeAmount;
+    }
+
+    public int getPrizeAmount() {
+        return prizeAmount;
+    }
+
+    public static LottoRank findRank(int matchedCount, boolean isBonusMatched, int prizeAmount) {
+        if(matchedCount == 6) return FIRST;
+        if(matchedCount == 5 && isBonusMatched) return SECOND;
+        if(matchedCount == 5) return THIRD;
+        if(matchedCount == 4) return FOURTH;
+        if(matchedCount == 3) return FIFTH;
+        return NONE;
+    }
+}
