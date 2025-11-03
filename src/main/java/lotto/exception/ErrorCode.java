1d7682f383d8c87a40932455024f91fbe56d3ee2
JJhwan7099 <ghks7099@naver.com>
2025-11-03 17:19:44 +0900

    feat(ErrorCode.java): LottoWinningNumbers에 필요한 형식 예외 추가

--- src/main/java/lotto/exception/ErrorCode.java
+++ src/main/java/lotto/exception/ErrorCode.java
@@ -1,14 +1,14 @@
 package lotto.exception;
 
 public enum ErrorCode {
-    LOTTO_NUMBER_FORMAT_INVALID("[ERROR] 로또 번호는 숫자 형태여야 합니다."),
     LOTTO_NUMBER_DUPLICATE("[ERROR] 로또 번호는 중복되지 않아야 합니다."),
     LOTTO_SIZE_INVALID("[ERROR] 로또 번호는 6개여야 합니다."),
     LOTTO_NUMBER_RANGE_INVALID("[ERROR] 로또 번호는 1~45 사이의 정수입니다."),
     BONUS_NUMBER_FORMAT_INVALID("[ERROR] 보너스 번호는 숫자 형태여야 합니다."),
     BONUS_NUMBER_RANGE_INVALID("[ERROR] 보너스 번호는 1~45 사이의 정수입니다."),
     BONUS_NUMBER_DUPLICATE("[ERROR] 보너스 번호와 당첨 번호는 중복될 수 없습니다."),
-    WINNING_NUMBER_FORMAT_INVALID("[ERROR] 로또 번호는 숫자 형태여야 합니다."),
+    WINNING_NUMBER_UNIT_FORMAT_INVALID("[ERROR] 당첨 로또 번호는 숫자 형태여야 합니다."),
+    WINNING_NUMBER_FORMAT_INVALID("[ERROR] 당첨 번호는 숫자와 쉼표로 구성된 형태여야 합니다."),
     WINNING_NUMBER_RANGE_INVALID("[ERROR] 로또 당첨 번호는 1~45 사이의 정수입니다."),
     WINNING_NUMBER_DUPLICATE("[ERROR] 당첨 번호는 중복되지 않아야 합니다."),
     WINNING_NUMBER_SIZE_INVALID("[ERROR] 당첨 번호는 6개 입니다."),
